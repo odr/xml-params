ac6fce1282ad85d037deb43ce1299320446f4bbe
Dmitry Olshansky <olshanskydr@gmail.com>
2018-11-30 12:11:06 +0300

    Upd: lts-11.22

--- Text/ParamsL/AntiParamsL.hs
+++ Text/ParamsL/AntiParamsL.hs
@@ -1,34 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 -- import qualified Data.ByteString as BS
-import Control.Monad.Trans.Resource(runResourceT)
-import Data.Conduit
-import qualified Data.Conduit.Binary as CB
+import           Conduit
+import           Control.Monad.Trans.Resource (runResourceT)
+import           Data.ByteString.Lazy.Internal (defaultChunkSize)
 import qualified Data.Conduit.List as CL
-import Data.String
-import System.Environment(getArgs)
+import           Data.Default (def)
+import           Data.String
+import           System.Environment (getArgs)
+import qualified System.IO as IO
+import           Text.ParamsL.ProcessParams (ParamsState(..), processParams)
 import qualified Text.XML.Stream.Parse as XP
 import qualified Text.XML.Stream.Render as XR
-import Data.Default(def)
 
-import Text.ParamsL.ProcessParams(processParams, ParamsState(..))
 
 main :: IO ()
 main = getArgs >>= doArgs
-    where
-        doArgs args = case args of
-            f:f2:_ -> parseFileRules f f2
-            _ -> error "Usage: antiParamsL <sourceFile> <resultFile>"
-                
-parseFileRules :: String -> String -> IO ()
-parseFileRules fn f2 = runResourceT $ 
-        XP.parseFile XP.def (fromString fn) 
-        $$ processParams def (ParamsState [] [] False) 
-        =$= CL.concatMap id 
-        =$= XR.prettify 
-        =$= XR.renderBytes XR.def 
-        =$= CB.sinkFile (fromString f2)
-    
+  where
+    doArgs args = case args of
+      f:f2:_ -> parseFileRules f f2
+      _      -> error "Usage: antiParamsL <sourceFile> <resultFile>"
 
+parseFileRules :: String -> String -> IO ()
+parseFileRules fn f2 = runResourceT $ runConduit
+  $ XP.parseFile XP.def (fromString fn)
+  .| processParams def (ParamsState [] [] False)
+  .| CL.concatMap id
+  .| CL.map Chunk
+  .| XR.prettify
+  .| XR.renderBuilderFlush XR.def
+  .| builderToByteStringWithFlush (allNewBuffersStrategy defaultChunkSize)
+  .|  bracketP (IO.openBinaryFile f2 IO.WriteMode) IO.hClose sinkHandleFlush
--- Text/ParamsL/ProcessParams.hs
+++ Text/ParamsL/ProcessParams.hs
@@ -1,59 +1,61 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Text.ParamsL.ProcessParams where
 
-import Control.Monad(when)
-import Data.Conduit(Conduit, await, leftover, yield)
-import Data.Default(Default(..))
-import Data.List(foldl', transpose)
-import Data.Maybe(fromMaybe)
-import Data.XML.Types(Event(..), Content(..), Name(..))
+import Control.Monad (when)
+import Data.Conduit
+import Data.Default (Default(..))
+import Data.List (foldl', transpose)
+import Data.Maybe (fromMaybe)
+import Data.XML.Types (Content(..), Event(..), Name(..))
 -- import Debug.Trace
 
-import Text.ParamsL.ParseList(ParamsL', applyParam, getParamsL, reverseParams, paramsToList)
+import Text.ParamsL.ParseList
+  (ParamsL', applyParam, getParamsL, paramsToList, reverseParams)
 
 -- | State of transformation
 data ParamsState = ParamsState {
-        globalParams    :: [ParamsL'],
+        globalParams :: [ParamsL'],
         -- ^ params outside of root element
-        localParams     :: [(ParamsL', [(ParamsL', [Event])])],
+        localParams  :: [(ParamsL', [(ParamsL', [Event])])],
         -- ^ params inside element with list of events for these params
-        inEval          :: Bool
+        inEval       :: Bool
         -- ^ if we are inside root element or not
     } deriving Show
 
 -- | Environment of transformation
-data ParamsEnv = ParamsEnv 
+data ParamsEnv = ParamsEnv
     { peIsParamsLElem :: Name -> Bool -- ^ define special element
     , peIsParamsLAttr :: Name -> Bool -- ^ define special attribute
     , peIsRoot        :: Name -> Bool -- ^ define root element
     }
 
 instance Default ParamsEnv where
-    def = ParamsEnv 
+    def = ParamsEnv
         { peIsParamsLElem   = (=="paramsL") . nameLocalName
         , peIsParamsLAttr   = (=="paramsL") . nameLocalName
         , peIsRoot          = (=="eval") . nameLocalName
         }
 
-processParams :: (Monad m) => ParamsEnv -> ParamsState -> Conduit Event m [Event]
-processParams pe0 ps0 = 
+processParams
+  :: (Monad m) => ParamsEnv -> ParamsState -> ConduitT Event [Event] m ()
+processParams pe0 ps0 =
         go (ps0, 0::Int)
     where
         go (ps,cnt) = await >>= \mx -> case mx of
-            Just (EventBeginElement n ats)  -> goBE n ats
-            Just e@(EventEndElement n)      -> goEE e n
-            Just e                          -> goEvent e
-            Nothing                         -> goEnd
+            Just (EventBeginElement n ats) -> goBE n ats
+            Just e@(EventEndElement n)     -> goEE e n
+            Just e                         -> goEvent e
+            Nothing                        -> goEnd
             where
                 goBE n ats
                     | rootElem n    = go (ps    { localParams = [   ( mconcat (getParams : globalParams ps)
                                                                     , [(mempty, cleanBE)]
                                                                     ) ]
-                                                , inEval = True 
+                                                , inEval = True
                                                 }, cnt)
-                    | inEval ps     = go (ps    { localParams = (getParams, [(mempty,cleanBE)]) : localParams ps 
+                    | inEval ps     = go (ps    { localParams = (getParams, [(mempty,cleanBE)]) : localParams ps
                                                 }, cnt)
-                    | otherwise     = yield cleanBE >> go (ps   { globalParams = getParams : globalParams ps 
+                    | otherwise     = yield cleanBE >> go (ps   { globalParams = getParams : globalParams ps
                                                                 }, cnt+1)
                     where
                         getParams :: ParamsL'
@@ -61,7 +63,7 @@ processParams pe0 ps0 =
                             where
                                 parseParams = getParamsL . foldl' (\acc c -> acc `mappend` showContent c) ""
                                 showContent (ContentText t) = t
-                                showContent _ = ""
+                                showContent _               = ""
 
                         notParamName = not . peIsParamsLAttr pe0 . fst -- (/="paramsL").nameLocalName.fst
                         cleanBE
@@ -70,27 +72,27 @@ processParams pe0 ps0 =
                 goEE e n
                     | rootElem n    = case localParams ps of
                         ((p,pes):_)     -> let ps' = ps {localParams = [], inEval = False} in do
-                                mapM_ yield   
-                                        $ reverse $ map (reverse . concat) 
-                                        $ transpose $ map (map snd) 
+                                mapM_ yield
+                                        $ reverse $ map (reverse . concat)
+                                        $ transpose $ map (map snd)
                                         $ calcParam p pes
-                                go (ps', cnt) 
+                                go (ps', cnt)
                         _               -> fail "Error in processParams (EventEndElement). LocalParams are empty on the end of eval."
-                    | inEval ps     = go (ps    { localParams = reduceParams $ localParams ps 
-                                                }, cnt) 
+                    | inEval ps     = go (ps    { localParams = reduceParams $ localParams ps
+                                                }, cnt)
                     | cnt == 0      = leftover e
                     | otherwise     = case globalParams ps of
                         (_:pps)         -> do
                             when (not $ isParamsL n) $ yield [e]
-                            go (ps { globalParams = pps }, cnt-1) 
+                            go (ps { globalParams = pps }, cnt-1)
                         _               -> fail "Error in processParams (EventEndElement). GlobalParams are empty but counter is more than 0."
                     where
                         calcParam :: ParamsL' -> [(ParamsL', [Event])] -> [[(ParamsL', [Event])]]
-                        calcParam _ [] 
+                        calcParam _ []
                             | null $ cleanEE = []
                             | otherwise = [[(mempty, cleanEE)]]
-                        calcParam p pes1@((pe1,es1):pes) = map (\(pe, es) -> 
-                                map (\p' -> let pe' = p' `mappend` pe in (pe', concat $ applyParam pe' es)) 
+                        calcParam p pes1@((pe1,es1):pes) = map (\(pe, es) ->
+                                map (\p' -> let pe' = p' `mappend` pe in (pe', concat $ applyParam pe' es))
                                 $ paramsToList p
                             ) pes2
                             where
@@ -98,32 +100,32 @@ processParams pe0 ps0 =
                                     | pe1 == mempty  = (pe1, cleanEE ++ es1) : pes
                                     | null cleanEE  = pes1
                                     | otherwise     = (mempty, cleanEE) : pes1
-                                    
+
                         cleanEE
                             | isParamsL n   = []
                             | otherwise     = [EventEndElement n]
                         reduceParams ((p, pes):(p2, pes2):lps) = (p2, pesIn ++ pes2) : lps
-                            where 
+                            where
                                 pesIn = concat $ transpose $ calcParam p pes
                         reduceParams _  = error "Error in processParams (EventEndElement). LocalParams are empty inside eval."
-                
+
                 goEvent e
                     | inEval ps = case localParams ps of
                         (p,[]) : lps            -> go (ps   { localParams = (p, [(mempty, [e])]) : lps
                                                             }, cnt)
-                        (p,pes0@((pe,es):pes)) : lps 
+                        (p,pes0@((pe,es):pes)) : lps
                             | pe == mempty      -> go (ps   { localParams = (p, (pe,e:es):pes) : lps
-                                                            }, cnt) 
+                                                            }, cnt)
                             | otherwise         -> go (ps   { localParams = (p, (mempty,[e]):pes0) : lps
                                                             }, cnt)
                         _                       -> fail "Error in processParams (some Event). LocalParams are empty inside eval."
                     | otherwise = yield [e] >> go (ps,cnt)
-            
+
                 goEnd
                     | inEval ps = fail "Error in processParams. It was closed inside eval. Unbalanced xml-tree"
                     | otherwise = return ()
 
-        isParamsL = peIsParamsLElem pe0 
+        isParamsL = peIsParamsLElem pe0
                 -- (`elem` ["{http://lukoil.ru/monitoring}paramsL", "{http://lukoil.ru/scheduler}paramsL"])
-        rootElem = peIsRoot pe0 
+        rootElem = peIsRoot pe0
                 -- (`elem` ["{http://lukoil.ru/scheduler}eval", "{http://lukoil.ru/scheduler}exec", "{http://lukoil.ru/monitoring}eval"])
